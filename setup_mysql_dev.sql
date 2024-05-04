e44b6095fd9175b777ed6551cfad1bc58a374261
Oloishorua <jacoboloishorua@gmail.com>
2024-05-04 04:43:56 +0300

    flush privilege command added

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,9 @@
---create db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- create new user named hbnb_dev_db
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+-- grant all privileges to user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+-- grant the SELECT privilege
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
