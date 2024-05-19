613b8944c21836bb0057a7d1ff94c797cfcaa6b1
Oloishorua <jacoboloishorua@gmail.com>
2024-05-20 02:00:50 +0300

    Task 7 correction

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,20 +7,20 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """ list of state ids"""
+
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
+
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """ teardown context"""
 
     if storage is not None:
         storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """ list of state ids"""
-
-    data = storage.all(State)
-    return render_template('7-states_list.html', total=data.values())
 
 
 if __name__ == '__main__':
