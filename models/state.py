5abd5dfd97b1e9cbd9cfd93ca8f405a1a8947a2d
Oloishorua <jacoboloishorua@gmail.com>
2024-05-19 20:42:31 +0300

    Task 7 - improve engines

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
 from models.base_model import BaseModel, Base
 import models
 from sqlalchemy import Column, String
@@ -10,7 +11,8 @@ storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
-    """ State class: class to represent states of cities"""
+    """ State class: represent states of cities"""
+
     if (storage_engine == 'db'):
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
@@ -20,8 +22,8 @@ class State(BaseModel, Base):
 
         @property
         def cities(self):
-            """cities list
-            """
+            """cities list"""
+
             result = []
             for j, i in models.storage.all(models.city.City).items():
                 if (i.state_id == self.id):
